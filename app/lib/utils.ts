ed2611d34849252217c8dda60128ace40a632b17
Manuel Raimann <raimannma@outlook.de>
2025-02-15 10:01:13 +0100

    Remove unused

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -25,31 +25,3 @@ export function snakeToPretty(str: string): string {
     .map((w) => w[0]?.toUpperCase() + w.slice(1))
     .join(" ");
 }
-
-type RetryOptions = {
-  retries: number;
-  retryDelay?: number;
-  timeout?: number;
-};
-
-export async function fetchWithRetry(
-  input: RequestInfo | URL,
-  init: RequestInit = {},
-  retryOptions: RetryOptions = { retries: 3, retryDelay: 100, timeout: 5000 },
-): Promise<Response> {
-  if (retryOptions.timeout) {
-    init.signal = AbortSignal.timeout(retryOptions.timeout);
-  }
-  return fetch(input, init)
-    .then(async (response) => (response.ok ? response : Promise.reject(response)))
-    .catch(async (err) => {
-      if (retryOptions.retries <= 0) {
-        console.error(`Failed to fetch ${input}: ${err}`);
-        throw err;
-      }
-      console.warn(`Failed to fetch ${input}: ${err}, ${retryOptions.retries} retries left`);
-      return await new Promise((resolve) => setTimeout(resolve, retryOptions.retryDelay ?? 0)).then(() =>
-        fetchWithRetry(input, init, { retries: retryOptions.retries - 1, retryDelay: retryOptions.retryDelay }),
-      );
-    });
-}
